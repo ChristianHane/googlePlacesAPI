260d069df30429cee54ce18cb79fdbf3e5afc41f
Christian <christian_hane@aol.com>
2018-01-20 11:13:24 -0800

    third

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 $("#search-butt").on("click", function() {
     
-    var queryURL = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=123+main+street&location=42.3675294,-71.186966&radius=10000&key=AIzaSyALmBnC7ujQhjXxBEIP1UCH3Lp5zFJLXb4"
+    var queryURL = "https://maps.googleapis.com/maps/api/place/textsearch/xml?query=123+main+street&location=42.3675294,-71.186966&radius=10000&key=AIzaSyALmBnC7ujQhjXxBEIP1UCH3Lp5zFJLXb4"
    
     $.ajax({
         url: queryURL,
